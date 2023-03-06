8d16d87a7f279cb716de567fb5c4b1e090150042
root <root@ip-172-31-22-21.us-east-2.compute.internal>
2023-03-06 02:44:28 +0000

    Restructuring the directories - additional changes

--- extension/iteration.py
+++
@@ -1,8 +0,0 @@
-# Time in mins - timeframe after which the extension will gather metric for each of the section as defined below.
-COLLECT_CONSUMPTION_DATA=60
-COLLECT_PROBLEM_DATA=1440
-COLLECT_FF_DATA=10080
-
-# Problem = Incident 
-#Time in ms - when problem is seen opened for the configured time below, treat it as incident. Default is 30 mins
-PROBLEM_INCIDENT=1800000
